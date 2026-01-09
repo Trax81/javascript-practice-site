23e2a4d95bba5cd92fb87e269432d8617176e532
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-01-09 08:37:17 -0800

    travis

--- javascript.js
+++ javascript.js
@@ -4,23 +4,40 @@
 
 function contact(event) {
     event.preventDefault();
-  //  emailjs
-   // .sendForm(
-   //     'service_jmnsl3i',
-   //     'template_9na30hz',
-   //     event.target,
-   //    'fkEzjp-UwKVb_Vr9G'
-  //  ).then(() => {
-  //      console.log('this worked')
-  //  })
-const loading = document.querySelector('.modal__overlay--loading');
-const success = document.querySelector('.modal__overlay--success');
-loading.classList += " modal__overlay--visible"
-setTimeout(() => {
-  loading.classList.remove("modal__overlay--visible")
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible" 
+
+
+    emailjs
+    .sendForm(
+       'service_jmnsl3i',
+        'template_9na30hz',
+        event.target,
+       'fkEzjp-UwKVb_Vr9G'
+    ).then(() => {
+       loading.classList.remove("modal__overlay--visible")
   success.classList += " modal__overlay--visible"
+       console.log('It worked 1')
+   }).catch(() =>{
+loading.classList.remove("modal__overlay--visible")
+    alert("The email service is temperarily unavailable. Please contact me directly on travantmedia@gmail.com");
+   })
+
+
+setTimeout(() => {
+ 
     console.log('it worked 1')
 }, 1000);
 
 
+}
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open")
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open"
 }
